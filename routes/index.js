938fadac313e7303d20525734aa20dca58e6b10a
annapancioli <annapancioli@gmail.com>
2019-03-21 03:27:54 -0400

    setup, images, first commit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-const data = require('../data');
+const data = require(`../data`);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Your first Express App', message: 'Express is actually pretty easy once you figure a few things out' });
-});
+router.get('/', function (req, res, next) {
+  console.log('data:', data);
 
-/* get a dynamic user via the route parameter */
-router.get('/:user', function(req, res, next) {
-  console.log('user:', data[req.params.user]);
-  res.render('userpage', data[req.params.user]);
+  res.render('index', data);
 });
 
 module.exports = router;
+
+/*var express = require('express');
+var router = express.Router();
+
+const data = require('../data');*/
+
+/* GET home page.
+router.get('/', function(req, res, next) {
+  console.log('data:', data);
+  res.render('index', data);
+}); */
+
+/* get a dynamic user via the route parameter
+router.get('/:user', function(req, res, next) {
+  console.log('user:', data[req.params.user]);
+  res.render('userpage', data[req.params.user]);
+}); */
+
+//module.exports = router;
